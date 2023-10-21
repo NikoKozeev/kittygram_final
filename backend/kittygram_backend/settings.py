37b90fd6bf3d023a95f788a931142b7520f234f3
Николай Козеев <kozeevniko@gmail.com>
2023-10-21 21:35:35 +0400

    config

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')
 
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1, localhost').split(', ')
 
